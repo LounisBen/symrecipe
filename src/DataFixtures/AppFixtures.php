026c1f87f5a37ee73eba6c653f4dfb9414b57c20
Lounis Benyahia <lounis.benyahia60@gmail.com>
2023-06-19 10:18:58 +0200

    Setting up login form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,14 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Ingredient;
-use App\Entity\Recipe;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use App\Entity\User;
 use Faker\Generator;
+use App\Entity\Recipe;
+use App\Entity\Ingredient;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
@@ -17,9 +19,12 @@ class AppFixtures extends Fixture
        
     private Generator $faker;
 
+    
+
     public function __construct()
     {
         $this->faker = Factory::create('fr_FR');
+        
     }
 
 
@@ -53,6 +58,19 @@ class AppFixtures extends Fixture
                 }
                 $manager->persist($recipe);
             }
+            
+            // Users
+            for ($i=0; $i < 10 ; $i++) { 
+                $user = new User();
+                $user->setFullName($this->faker->name())
+                    ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
+                    ->setEmail($this->faker->email())
+                    ->setRoles(['ROLE_USER'])
+                    ->setPlainPassword('password');
+                
+            $manager->persist($user);
+            }
+            
             $manager->flush();
     }
 }
